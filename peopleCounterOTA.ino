6f0d3f60c531c3c20499c35cee10fc6c58122f4b
Andrea Fox <42143557+Andrea-Fox@users.noreply.github.com>
2020-10-24 18:52:28 +0200

    Add files via upload
    Version which also allows OTA updates

---
+++ peopleCounterOTA.ino
@@ -0,0 +1,340 @@
+#include <Wire.h>
+#include "SparkFun_VL53L1X.h"
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+const char* ssid = "";     //wi-fi netwrok name
+const char* password = "";  //wi-fi network password
+const char* mqtt_server = "";   // mqtt broker ip address (without port)
+const int mqtt_port = 1883;                   // mqtt broker port
+const char *mqtt_user = "";
+const char *mqtt_pass = "";
+#define mqtt_serial_publish_ch "peopleCounter/serialdata/tx"
+#define mqtt_serial_publish_distance_ch "peopleCounterDistance/serialdata/tx"
+#define mqtt_serial_receiver_ch "peopleCounter/serialdata/rx"
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+char peopleCounterArray[50];
+
+
+//Optional interrupt and shutdown pins.  Vanno cambiati e messi quelli che hanno i collegamenti i^2C
+#define SHUTDOWN_PIN 2    
+#define INTERRUPT_PIN 3
+
+SFEVL53L1X distanceSensor(Wire);//, SHUTDOWN_PIN, INTERRUPT_PIN);
+
+static int NOBODY = 0;
+static int SOMEONE = 1;
+static int LEFT = 0;
+static int RIGHT = 1;
+
+static int DIST_THRESHOLD_MAX[] = {2000, 2000};
+
+static int PathTrack[] = {0,0,0,0};
+static int PathTrackFillingSize = 1; // init this to 1 as we start from state where nobody is any of the zones
+static int LeftPreviousStatus = NOBODY;
+static int RightPreviousStatus = NOBODY;
+static int PeopleCount = 0;
+//195,60
+//167, 231
+static int center[2] = {167, 231}; // {21, 85}; these are the spad center of the 2 8*16 zones */  //239, 175, bassi: 35, 99
+// 175: quasi sempre sopra i due metri, mai sotto i 1800
+// 239: quasi sempre tra i 1500 e i 2000
+static int Zone = 0;
+static int PplCounter = 0;
+
+unsigned long previousMillis = 0;  // will store last time LED was updated
+const long interval = 50;  // interval at which to blink (milliseconds)
+
+
+void setup_wifi() 
+{
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+    WiFi.begin(ssid, password);
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str(),mqtt_user,mqtt_pass)) {
+      Serial.println("connected");
+      //Once connected, publish an announcement...
+      //publishSerialData(PeopleCount);
+      // ... and resubscribe
+      client.subscribe(mqtt_serial_receiver_ch);
+      //publishSerialData(0);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
+void publishSerialLedStatus(char *serialData){
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.publish(mqtt_serial_publish_ch_light, serialData);
+}
+
+void publishSerialData(int serialData){
+  //serialData = max(0, serialData);
+  if (!client.connected()) {
+    reconnect();
+  }
+  String stringaCounter = String(serialData);
+  stringaCounter.toCharArray(peopleCounterArray, stringaCounter.length() +1);
+  client.publish(mqtt_serial_publish_ch, peopleCounterArray);
+  delay(50);
+  serialData = 0;
+  stringaCounter = String(serialData);
+  stringaCounter.toCharArray(peopleCounterArray, stringaCounter.length() +1);
+  client.publish(mqtt_serial_publish_ch, peopleCounterArray);
+}
+
+
+void publishSerialDataDistanza(int serialData){
+  //serialData = max(0, serialData);
+  if (!client.connected()) {
+    reconnect();
+  }
+  String stringaCounter = String(serialData);
+  stringaCounter.toCharArray(peopleCounterArray, stringaCounter.length() +1);
+  client.publish(mqtt_serial_publish_distance_ch, peopleCounterArray);
+}
+
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  // ArduinoOTA.setHostname("myesp32");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Wire.begin();
+
+  Serial.begin(9600);
+  Serial.println("VL53L1X Qwiic Test");
+
+  if (distanceSensor.init() == false)
+    Serial.println("Sensor online!");
+  distanceSensor.setIntermeasurementPeriod(100);
+  distanceSensor.setDistanceModeLong();
+
+ 
+  Serial.setTimeout(500);// Set time out for setup_wifi();
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+  delay(1000);
+  publishSerialData(0);
+  reconnect();  
+  
+}
+
+
+
+void loop(void)
+{
+  ArduinoOTA.handle();
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    uint16_t distance;
+    client.loop();
+    if (!client.connected()) 
+    {
+      reconnect();
+    }
+    
+    
+    distanceSensor.setROI(8, 8, center[Zone]);  // primo: altezza, secondo: larghezza
+    
+    distanceSensor.setTimingBudgetInMs(50);
+    distanceSensor.startRanging(); //Write configuration bytes to initiate measurement
+    distance = distanceSensor.getDistance(); //Get the result of the measurement from the sensor
+    distanceSensor.stopRanging();
+  
+    Serial.println(distance);
+    publishSerialDataDistanza(distance);
+    
+  
+     // inject the new ranged distance in the people counting algorithm
+    PplCounter = ProcessPeopleCountingData(distance, Zone);
+  
+    Zone++;
+    Zone = Zone%2;
+
+  }
+
+}
+
+// NOBODY = 0, SOMEONE = 1, LEFT = 0, RIGHT = 1
+// contatore zona sinistra: 1, contatore zona destra: 10
+
+int ProcessPeopleCountingData(int16_t Distance, uint8_t zone) {
+
+    int CurrentZoneStatus = NOBODY;
+    int AllZonesCurrentStatus = 0;
+    int AnEventHasOccured = 0;
+
+  if (Distance < DIST_THRESHOLD_MAX[Zone]) {
+    // Someone is in !
+    CurrentZoneStatus = SOMEONE;
+  }
+
+  // left zone
+  if (zone == LEFT) {
+
+    if (CurrentZoneStatus != LeftPreviousStatus) {
+      // event in left zone has occured
+      AnEventHasOccured = 1;
+
+      if (CurrentZoneStatus == SOMEONE) {
+        AllZonesCurrentStatus += 1;
+      }
+      // need to check right zone as well ...
+      if (RightPreviousStatus == SOMEONE) {
+        // event in left zone has occured
+        AllZonesCurrentStatus += 2;
+      }
+      // remember for next time
+      LeftPreviousStatus = CurrentZoneStatus;
+    }
+  }
+  // right zone
+  else {
+
+    if (CurrentZoneStatus != RightPreviousStatus) {
+
+      // event in left zone has occured
+      AnEventHasOccured = 1;
+      if (CurrentZoneStatus == SOMEONE) {
+        AllZonesCurrentStatus += 2;
+      }
+      // need to left right zone as well ...
+      if (LeftPreviousStatus == SOMEONE) {
+        // event in left zone has occured
+        AllZonesCurrentStatus += 1;
+      }
+      // remember for next time
+      RightPreviousStatus = CurrentZoneStatus;
+    }
+  }
+
+  // if an event has occured
+  if (AnEventHasOccured) {
+    if (PathTrackFillingSize < 4) {
+      PathTrackFillingSize ++;
+    }
+
+    // if nobody anywhere lets check if an exit or entry has happened
+    if ((LeftPreviousStatus == NOBODY) && (RightPreviousStatus == NOBODY)) {
+
+      // check exit or entry only if PathTrackFillingSize is 4 (for example 0 1 3 2) and last event is 0 (nobobdy anywhere)
+      if (PathTrackFillingSize == 4) {
+        // check exit or entry. no need to check PathTrack[0] == 0 , it is always the case
+        Serial.println();
+        if ((PathTrack[1] == 1)  && (PathTrack[2] == 3) && (PathTrack[3] == 2)) {
+          // This an entry
+          PeopleCount ++;
+          Serial.print("Una persona è entrata. Persone attualmente nella stanza: ");
+          Serial.print(PeopleCount);
+          publishSerialData(1);   // 1 è il segnale che rappresenta che una persona è entrata
+        } else if ((PathTrack[1] == 2)  && (PathTrack[2] == 3) && (PathTrack[3] == 1)) {
+          // This an exit
+          PeopleCount --;
+          Serial.print("Una persona è uscita. Persone attualmente nella stanza: ");
+          Serial.print(PeopleCount);  // 2 è il segnale che segna che una persona è uscita
+          publishSerialData(2);
+          }
+      }
+      for (int i=0; i<4; i++){
+        PathTrack[i] = 0;
+      }
+      PathTrackFillingSize = 1;
+    }
+    else {
+      // update PathTrack
+      // example of PathTrack update
+      // 0
+      // 0 1
+      // 0 1 3
+      // 0 1 3 1
+      // 0 1 3 3
+      // 0 1 3 2 ==> if next is 0 : check if exit
+      PathTrack[PathTrackFillingSize-1] = AllZonesCurrentStatus;
+    }
+  }
+
+  // output debug data to main host machine
+  return(PeopleCount);     
+}
